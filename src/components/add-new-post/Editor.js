33663808e1442fb790c06d5e33211d26d1f5515e
vinhtp1 <doggogogogogo@gmail.com>
2019-07-19 17:20:53 +0700

    editor fixed

--- src/components/add-new-post/Editor.js
+++ src/components/add-new-post/Editor.js
@@ -7,28 +7,32 @@ import "../../assets/quill.css";
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
+import MyUploadAdapter from '../../commons/MyUploadAdapter';
+
 const Editor = () => (
   <Card small className="mb-3">
     <CardBody>
       <Form className="add-new-post">
         <FormInput size="lg" className="mb-3" placeholder="Your Post Title" />
         <CKEditor className="add-new-post__editor mb-1"
-                    editor={ ClassicEditor }
-                    onInit={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ editor => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ editor => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
+          editor={ClassicEditor}
+          onInit={editor => {
+              editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+                return new MyUploadAdapter(loader);
+              };
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={editor => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={editor => {
+            console.log('Focus.', editor);
+          }}
+        />
       </Form>
     </CardBody>
   </Card>
@@ -36,3 +40,4 @@ const Editor = () => (
 
 
 export default Editor;
+
